f8475ba8c380b3c09b6816405b7fcfd197e3b2f5
ilgulee <iglee2000ca@gmail.com>
2017-09-15 20:17:35 -0400

    Finished Lab2

---
+++ New Unity Project 1/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+    public GameObject[] bodies;
+    public GameObject[] buttons;
+    // Use this for initialization
+    void Start() {
+        bodies = GameObject.FindGameObjectsWithTag("Player");
+        buttons = GameObject.FindGameObjectsWithTag("GameController");
+
+        foreach (var a in buttons)
+        {
+            print(a.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        foreach (var body in bodies)
+        {
+            body.SetActive(false);
+        }
+
+        if (this.name == "btnRed")
+        {
+            bodies[2].SetActive(true);
+        } else if (this.name == "btnBlue")
+        {
+            bodies[1].SetActive(true);
+        } else if (this.name == "btnYellow")
+        {
+            bodies[0].SetActive(true);
+        }
+    }
+}
